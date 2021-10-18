2b55f6a15ed5874025f4b7d683b5d2184cc7e1a1
emhuammedb <MUENBI@users.noreply.github.com>
2021-10-19 00:30:13 +0300

    jscamp-181021

---
+++ src/app.js
@@ -0,0 +1,19 @@
+console.log("Hello Kodlama.io");
+
+let dollarYesterday = 9.2;
+let dollarToday = 9.3;
+
+let konutKredileri = [
+  "Konut Kredisi",
+  "Emlak Konut Kredisi",
+  "Kamu Konut Kredisi",
+  "Özel Konut Kredisi",
+];
+
+console.log("<ul>");
+for (let i = 0; i < konutKredileri.length; i++) {
+  console.log("<li>" + konutKredileri[i] + "</li>");
+}
+console.log("</ul>");
+
+console.log(dollarYesterday);
